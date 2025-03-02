c93e0af7e57a60bfa9926b776e19aff52f06c0d5
Nathan J <nathanjobin@outlook.com>
2025-03-02 15:36:52 -0500

    Modification BibliothequeService et EmprunteurService pour la gestionb des documents et des emprunteurs

--- src/main/java/ca/cal/tp2/Service/BibliothequeService.java
+++ src/main/java/ca/cal/tp2/Service/BibliothequeService.java
@@ -1,114 +1,79 @@
 package ca.cal.tp2.Service;
 
 import ca.cal.tp2.Modele.*;
-import ca.cal.tp2.Persistance.JDBC.BibliothequeRepositoryJDBC;
-import ca.cal.tp2.Persistance.PersistanceGenerique;
+import ca.cal.tp2.Persistance.EmprunteurRepository;
+import ca.cal.tp2.Persistance.DocumentRepository;
 import ca.cal.tp2.Service.dto.DocumentDTO;
-import ca.cal.tp2.Service.dto.LivreDTO;
+import ca.cal.tp2.Service.dto.EmprunteurDTO;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class BibliothequeService {
 
-    private final PersistanceGenerique persistanceGenerique;
-    private final Map<Long, Emprunteur> emprunteurs;
+    private final DocumentRepository livreRepository;
+    private final DocumentRepository cdRepository;
+    private final DocumentRepository dvdRepository;
+    private final EmprunteurRepository emprunteurRepository;
 
-    public BibliothequeService(PersistanceGenerique persistanceGenerique) {
-        this.persistanceGenerique = persistanceGenerique;
-        this.emprunteurs = new HashMap<>();
+    public BibliothequeService(DocumentRepository livreRepository, DocumentRepository cdRepository, DocumentRepository dvdRepository, EmprunteurRepository emprunteurRepository) {
+        this.livreRepository = livreRepository;
+        this.cdRepository = cdRepository;
+        this.dvdRepository = dvdRepository;
+        this.emprunteurRepository = emprunteurRepository;
     }
 
-    public void ajouterEmprunteur(Emprunteur emprunteur){
-        emprunteurs.put(emprunteur.getId(), emprunteur);
+    public List<Document> getDocuments(String titre, String auteur, Integer annee) {
+        List<Document> resultats = new ArrayList<>();
+        resultats.addAll(livreRepository.rechercheDocuments(titre, auteur, annee));
+        resultats.addAll(cdRepository.rechercheDocuments(titre, auteur, annee));
+        resultats.addAll(dvdRepository.rechercheDocuments(titre, auteur, annee));
+        return resultats;
     }
 
-    public Document getDocumentParId(long documentId) {
-        return (Document) persistanceGenerique.getById(documentId);
+    public List<DocumentDTO> rechercherDocuments(String titre, String auteur, Integer annee) {
+        List<Document> documents = getDocuments(titre, auteur, annee);
+        List<DocumentDTO> resultats = new ArrayList<>();
+        for (Document document : documents) {
+            resultats.add(DocumentDTO.toDto(document));
+        }
+        return resultats;
     }
 
-    public DocumentDTO rechercherDocumentParId(long documentId) {
-        return DocumentDTO.toDto((Document) persistanceGenerique.getById(documentId));
+    public Emprunteur getEmprunteurParEmail(String email) {
+        return emprunteurRepository.getByEmail(email);
     }
 
-    public Emprunteur getEmprunteurParId(long idEmprunteur) {
-        return emprunteurs.get(idEmprunteur);
+    public EmprunteurDTO rechercherEmprunteurParEmail(String email) {
+        return EmprunteurDTO.toDto(getEmprunteurParEmail(email));
     }
 
     public List<Emprunteur> getTousEmprunteurs() {
-        return new ArrayList<>(emprunteurs.values());
+        return emprunteurRepository.getAll();
     }
 
-    /*
-    public List<Document> rechercherDocuments(String titre, String auteur, String editeur, String type) {
-        List<Document> resultats = new ArrayList<>();
-        for (Document document : documents.values()) {
-            boolean correspond = true;
-            if (titre != null && !document.getTitre().equalsIgnoreCase(titre)) {
-                correspond = false;
-            }
-            if (auteur != null && document instanceof Livre && !((Livre) document).getAuteur().equalsIgnoreCase(auteur)) {
-                correspond = false;
-            }
-            if (editeur != null && document instanceof Livre && !((Livre) document).getEditeur().equalsIgnoreCase(editeur)) {
-                correspond = false;
-            }
-            if (type != null && !type.isEmpty() && !document.getClass().getSimpleName().equalsIgnoreCase(type)) {
-                correspond = false;
-            }
-            if (correspond) {
-                resultats.add(document);
-            }
-        }
-        afficherResultatsRecherche(resultats);
-        return resultats;
-    }*/
-
-    public void afficherResultatsRecherche(List<Document> documents) {
-        for (Document document : documents) {
-            System.out.println("---------------------------------");
-            System.out.println("ID: " + document.getId());
-            System.out.println("Titre: " + document.getTitre());
-            System.out.println("Année de publication: " + document.getAnneePublication());
-            System.out.println("Nombre d'exemplaires: " + document.getNombreExemplaires());
-            System.out.println("Type: " + document.getClass().getSimpleName());
-            if (document instanceof Livre) {
-                System.out.println("Auteur: " + ((Livre) document).getAuteur());
-                System.out.println("Éditeur: " + ((Livre) document).getEditeur());
-                System.out.println("Durée d'emprunt: 3 semaines");
-            } else if (document instanceof Cd) {
-                System.out.println("Artiste: " + ((Cd) document).getArtiste());
-                System.out.println("Durée d'emprunt: 2 semaines");
-            } else if (document instanceof Dvd) {
-                System.out.println("Réalisateur: " + ((Dvd) document).getRealisateur());
-                System.out.println("Durée d'emprunt: 1 semaine");
-            }
-            System.out.println("Disponible: " + (verifierQuantiteDocuments(document.getId()) > 0 ? "Oui" : "Non"));
-            System.out.println();
-        }
-    }
-
-    public int verifierQuantiteDocuments(long documentIdRecherche) {
+    public int verifierQuantiteDocuments(String titre, String auteur, Integer annee) {
         List<Emprunteur> emprunteurs = getTousEmprunteurs();
         int borrowedCount = 0;
+
+        List<Document> documents = getDocuments(titre, auteur, annee);
+        if (documents.isEmpty()) {
+            throw new RuntimeException("Document non trouvé: " + titre);
+        }
+
+        Document document = documents.get(0); // Assuming titles are unique
+        long documentId = document.getId();
 
         for (Emprunteur emprunteur : emprunteurs) {
             for (Emprunt emprunt : emprunteur.getEmprunts()) {
                 for (EmpruntDetail empruntDetail : emprunt.getEmpruntDetails()) {
-                    if (empruntDetail.getLineItemID() == documentIdRecherche) {
+                    if (empruntDetail.getDocument().getId() == documentId) {
                         if (empruntDetail.getDateRetourActuelle() == null) {
                             borrowedCount++;
                         }
                     }
                 }
             }
-        }
-
-        Document document = getDocumentParId(documentIdRecherche);
-        if (document == null) {
-            throw new RuntimeException("Document non trouvé: " + documentIdRecherche);
         }
 
         int availableCount = document.getNombreExemplaires() - borrowedCount;
--- src/main/java/ca/cal/tp2/Service/EmprunteurService.java
+++ src/main/java/ca/cal/tp2/Service/EmprunteurService.java
@@ -1,22 +1,23 @@
 package ca.cal.tp2.Service;
 
 import ca.cal.tp2.Modele.*;
+import ca.cal.tp2.Persistance.DocumentRepository;
+import ca.cal.tp2.Persistance.EmpruntRepository;
 
 import java.time.LocalDate;
 import java.util.*;
 
 public class EmprunteurService {
     private final BibliothequeService bibliothequeService;
+    private final EmpruntRepository empruntRepository;
 
-    // Variable statique pour générer l'ID d'emprunt
-    private static long prochainIdEmprunt = 1;
-
-    public EmprunteurService(BibliothequeService bibliothequeService) {
+    public EmprunteurService(EmpruntRepository empruntRepository, BibliothequeService bibliothequeService) {
         this.bibliothequeService = bibliothequeService;
+        this.empruntRepository = empruntRepository;
     }
 
-    public void emprunterDocuments(long emprunteurId, List<Long> documentIds, int annee, int mois, int jour) {
-        Emprunteur emprunteur = bibliothequeService.getEmprunteurParId(emprunteurId);
+    public void emprunterDocuments(String emprunteurEmail, List<String> documentTitres, int annee, int mois, int jour) {
+        Emprunteur emprunteur = bibliothequeService.getEmprunteurParEmail(emprunteurEmail);
         LocalDate dateEmprunt = LocalDate.of(annee, mois, jour);
 
         if (emprunteur == null) {
@@ -29,82 +30,26 @@ public class EmprunteurService {
             }
         }
 
-        Emprunt emprunt = new Emprunt(prochainIdEmprunt++, dateEmprunt);
-        for (Long documentId : documentIds) {
-            Document document = bibliothequeService.getDocumentParId(documentId);
+        Emprunt emprunt = new Emprunt(0, dateEmprunt, emprunteur);
+        for (String documentTitre : documentTitres) {
+            for (Document document : bibliothequeService.getDocuments(documentTitre, null, null)) {
 
-            if (document == null) {
-                throw new RuntimeException("Document non trouvé");
+                if (document == null) {
+                    throw new RuntimeException("Document non trouvé");
+                }
+
+                if (bibliothequeService.verifierQuantiteDocuments(documentTitre, null, null) <= 0) {
+                    throw new RuntimeException("Plus d'exemplaire empruntable pour le document, titre: " + document.getTitre());
+                }
+
+                LocalDate dateRetourPrevue = calculerDateRetour(document, dateEmprunt);
+                EmpruntDetail empruntDetail = new EmpruntDetail(0, dateRetourPrevue, emprunt, document);
+                emprunt.ajouterEmpruntDetail(empruntDetail);
             }
-
-            if (bibliothequeService.verifierQuantiteDocuments(documentId) <= 0) {
-                throw new RuntimeException("Plus d'exemplaire empruntable pour le document, id: " + document.getId() + ", titre: " + document.getTitre());
-            }
-
-            LocalDate dateRetourPrevue = calculerDateRetour(document, dateEmprunt);
-            EmpruntDetail empruntDetail = new EmpruntDetail(documentId, dateRetourPrevue);
-            emprunt.ajouterEmpruntDetail(empruntDetail);
         }
 
         emprunteur.getEmprunts().add(emprunt);
-    }
-
-    public void retournerDocument(long emprunteurId, long documentId, int annee, int mois, int jour) {
-        Emprunteur emprunteur = bibliothequeService.getEmprunteurParId(emprunteurId);
-        LocalDate dateRetour = LocalDate.of(annee, mois, jour);
-
-        if (emprunteur == null) {
-            throw new RuntimeException("Emprunteur non trouvé");
-        }
-
-        for (Emprunt emprunt : emprunteur.getEmprunts()) {
-            for (EmpruntDetail empruntDetail : emprunt.getEmpruntDetails()) {
-                if (empruntDetail.getLineItemID() == documentId && empruntDetail.getDateRetourActuelle() == null) {
-                    empruntDetail.setDateRetourActuelle(dateRetour);
-
-                    // Calculer l'amende si le document est retourné en retard
-                    if (dateRetour.isAfter(empruntDetail.getDateRetourPrevue())) {
-                        long differenceEnJours = java.time.temporal.ChronoUnit.DAYS.between(empruntDetail.getDateRetourPrevue(), dateRetour);
-                        double montantAmende = differenceEnJours * 0.25;
-
-                        Amendes amende = new Amendes(System.currentTimeMillis(), montantAmende, dateRetour);
-                        emprunteur.getAmendes().add(amende);
-                        empruntDetail.setStatus("retourné en retard");
-                    } else {
-                        empruntDetail.setStatus("retourné");
-                    }
-                    return;
-                }
-            }
-        }
-
-        throw new RuntimeException("Document non trouvé dans les emprunts de l'emprunteur");
-    }
-
-    public void payerAmendes(double montant, long emprunteurId, int annee, int mois, int jour) {
-        Emprunteur emprunteur = bibliothequeService.getEmprunteurParId(emprunteurId);
-        if (emprunteur == null) {
-            throw new RuntimeException("Emprunteur non trouvé");
-        }
-
-        double montantRestant = montant;
-        LocalDate datePaiement = LocalDate.of(annee, mois, jour);
-
-        for (Amendes amende : emprunteur.getAmendes()) {
-            if (!amende.isPaye() && montantRestant > 0) {
-                if (montantRestant >= amende.getMontant()) {
-                    amende.setPaye(true);
-                    amende.setDatePaye(datePaiement);
-                    montantRestant -= amende.getMontant();
-                } else {
-                    throw new RuntimeException("Montant insuffisant pour payer l'amende de " + amende.getMontant());
-                }
-            }
-        }
-
-        if (montantRestant > 0) {
-            System.out.println("Montant restant après paiement des amendes: " + montantRestant + "\n");
-        }
+        empruntRepository.save(emprunt);
     }
 
     private LocalDate calculerDateRetour(Document document, LocalDate dateEmprunt) {
@@ -119,64 +64,5 @@ public class EmprunteurService {
         }
 
         return dateRetourPrevue;
-    }
-
-    public void afficherEmprunts(long emprunteurId) {
-        Emprunteur emprunteur = bibliothequeService.getEmprunteurParId(emprunteurId);
-
-        if (emprunteur == null) {
-            throw new RuntimeException("Emprunteur non trouvé");
-        }
-
-        for (Emprunt emprunt : emprunteur.getEmprunts()) {
-            System.out.println("---------------------------------");
-            System.out.println("Emprunt ID: " + emprunt.getId());
-            System.out.println("Date d'emprunt: " + emprunt.getEmprunt());
-            System.out.println("Documents empruntés:");
-            for (EmpruntDetail empruntDetail : emprunt.getEmpruntDetails()) {
-                Document document = bibliothequeService.getDocumentParId(empruntDetail.getLineItemID());
-                System.out.println(" - Titre: " + document.getTitre() + ", Année: " + document.getAnneePublication() + ", Date de retour prévue: " + empruntDetail.getDateRetourPrevue());
-                System.out.println("   Statut: " + empruntDetail.getStatus());
-                if (empruntDetail.getDateRetourActuelle() != null) {
-                    System.out.println("   Date de retour actuelle: " + empruntDetail.getDateRetourActuelle());
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    public void afficherInfoEmprunteur(long emprunteurId) {
-        System.out.println("---------------------------------");
-        Emprunteur emprunteur = bibliothequeService.getEmprunteurParId(emprunteurId);
-
-        if (emprunteur == null) {
-            throw new RuntimeException("Emprunteur non trouvé");
-        }
-        System.out.println("Emprunteur ID: " + emprunteur.getId());
-        System.out.println("Nom: " + emprunteur.getNom());
-        System.out.println("Prénom: " + emprunteur.getPrenom());
-        System.out.println("Emprunts en cours:");
-        for (Emprunt emprunt : emprunteur.getEmprunts()) {
-            System.out.println(" - Emprunt ID: " + emprunt.getId() + ", Date d'emprunt: " + emprunt.getEmprunt());
-            for (EmpruntDetail empruntDetail : emprunt.getEmpruntDetails()) {
-                Document document = bibliothequeService.getDocumentParId(empruntDetail.getLineItemID());
-                System.out.println("   - Titre: " + document.getTitre() + ", Date de retour prévue: " + empruntDetail.getDateRetourPrevue());
-                System.out.println("     Statut: " + empruntDetail.getStatus());
-                if (empruntDetail.getDateRetourActuelle() != null) {
-                    System.out.println("     Date de retour actuelle: " + empruntDetail.getDateRetourActuelle());
-                }
-            }
-        }
-
-        System.out.println("Amendes:");
-        for (Amendes amende : emprunteur.getAmendes()) {
-            System.out.println(" - Montant: " + amende.getMontant() + ", Date émise: " + amende.getDateEmis());
-            if (amende.isPaye()) {
-                System.out.println("   Statut: Payée, Date de paiement: " + amende.getDatePaye());
-            } else {
-                System.out.println("   Statut: Non payée");
-            }
-        }
-        System.out.println();
     }
 }
