c994fed8f34aebba04991f7d4c92938ba5c6f377
Nathan J <nathanjobin@outlook.com>
2025-03-05 14:39:22 -0500

    Refactor de EmprunteurRepository: retrait de la method getAll et modification de getByEmail

--- src/main/java/ca/cal/tp2/Persistance/EmprunteurRepository.java
+++ src/main/java/ca/cal/tp2/Persistance/EmprunteurRepository.java
@@ -7,5 +7,4 @@ import java.util.List;
 public interface EmprunteurRepository {
     void save(Emprunteur emprunteur);
     Emprunteur getByEmail(String email);
-    List<Emprunteur> getAll();
 }
--- src/main/java/ca/cal/tp2/Persistance/EmprunteurRepositoryJPA.java
+++ src/main/java/ca/cal/tp2/Persistance/EmprunteurRepositoryJPA.java
@@ -1,7 +1,7 @@
-package ca.cal.tp2.Persistance.JPA;
+package ca.cal.tp2.Persistance;
 
+import ca.cal.tp2.Modele.Emprunt;
 import ca.cal.tp2.Modele.Emprunteur;
-import ca.cal.tp2.Persistance.EmprunteurRepository;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -23,33 +23,23 @@ public class EmprunteurRepositoryJPA implements EmprunteurRepository {
     @Override
     public Emprunteur getByEmail(String email) {
         try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            TypedQuery<Emprunteur> query = entityManager.createQuery("SELECT e FROM Emprunteur e WHERE e.email = :email", Emprunteur.class);
+            entityManager.getTransaction().begin();
+            var sql = """
+                SELECT e FROM Emprunteur e 
+                left join fetch e.emprunts em
+                left join fetch e.amendes
+                left join fetch em.empruntDetails
+                WHERE e.email = :email
+            """;
+            TypedQuery<Emprunteur> query = entityManager.createQuery(sql, Emprunteur.class);
             query.setParameter("email", email);
             Emprunteur emprunteur = query.getSingleResult();
-
-            emprunteur.getAmendes().size();
-            emprunteur.getEmprunts().forEach(emprunt -> emprunt.getEmpruntDetails().size());
-
+            entityManager.getTransaction().commit();
             return emprunteur;
         } catch (NoResultException e) {
             return null;
         } catch (RuntimeException e) {
             throw new RuntimeException("Erreur lors de la récupération de l'Emprunteur par email: " + e.getMessage(), e);
-        }
-    }
-
-    @Override
-    public List<Emprunteur> getAll() {
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            TypedQuery<Emprunteur> query = entityManager.createQuery("SELECT e FROM Emprunteur e", Emprunteur.class);
-            List<Emprunteur> emprunteurs = query.getResultList();
-            for (Emprunteur emprunteur : emprunteurs) {
-                emprunteur.getAmendes().size();
-                emprunteur.getEmprunts().forEach(emprunt -> emprunt.getEmpruntDetails().size());
-            }
-            return emprunteurs;
-        } catch (RuntimeException e) {
-            throw new RuntimeException("Erreur lors de la récupération de tous les Emprunteurs : " + e.getMessage(), e);
         }
     }
 }
